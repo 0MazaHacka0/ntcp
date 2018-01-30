a04f4043c8ad135501f75eb0f1a9904a779d10d0
0MazaHacka0 <killerinshadow2@gmail.com>
2018-01-30 18:11:01 +0300

    update network

--- network.py
+++ network.py
@@ -9,25 +9,52 @@ import random
 import glob
 import sys
 import io
-import tensorflow as tf
 import pylab as plt
 
-
 from keras.callbacks import History
-history = History()
-
-# Configure TensorBoard callback func
-tbCallBack = callbacks.TensorBoard(log_dir='./logs', histogram_freq=1, batch_size=128, write_graph=True, write_grads=True, write_images=False, embeddings_freq=1, embeddings_layer_names=None, embeddings_metadata=None)
 
 def findFiles(path): return glob.glob(path)
+
+# Settings
+# TensorBoard logdir
+tb_log_dir = './logs'
+
+# Filename for out file
+out_filename = 'out_generated.txt'
+
+# Setting up epoch and iterations
+# We will print generated text %iterations% times after every %epochs%
+epochs = 10
+iterations = 3
+
+layers_count = 5
+
+# Count of chars to generate every iteration
+count_of_chars = 800
+
+# History callback for matplotlib
+history = History()
+
+# TensorBoard Callback
+tbCallBack = callbacks.TensorBoard(log_dir=tb_log_dir, histogram_freq=1, batch_size=128, write_graph=True,
+                                   write_grads=True, write_images=False, embeddings_freq=1, embeddings_layer_names=None,
+                                   embeddings_metadata=None)
+
+# Workaround to solve problem saving history after every epoch
+All_history = {
+    'acc': list(),
+    'val_acc': list(),
+    'loss': list(),
+    'val_loss': list()
+}
 
 sentence = ''
 
 # Write generated passwords
-out = io.open('out_generated', 'w')
+out = io.open(out_filename, 'a')
 
 # Read data
-path = 'data/*.txt'
+path = 'data/*.*'
 text = ''
 
 print('Proceed files')
@@ -36,15 +63,17 @@ for filename in findFiles(path):
     old_len = len(text)
     text += io.open(filename, encoding='cp1251').read()
     print('corpus lenght: ', len(text) - old_len)
+    print()
+print('All files lenght is:', len(text))
 
-# Получаем набор символов
+# Getting set of chars for vectors
 chars = sorted(list(set(text)))
 print('total num of chars set:', len(chars))
 char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
 # cut the text in semi-redundant sequences of maxlen characters
-maxlen = 20
+maxlen = 24
 step = 3
 sentences = []
 next_chars = []
@@ -61,16 +90,18 @@ for i, sentence in enumerate(sentences):
         x[i, t, char_indices[char]] = 1
     y[i, char_indices[next_chars[i]]] = 1
 
-# build the model: a single LSTM
+# Build the model
 print('Build model...')
 model = Sequential()
-model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(LSTM(128, input_shape=(maxlen, len(chars)), return_sequences=True))
+for i in range(layers_count - 1):
+    model.add(LSTM(128, return_sequences=True))
+model.add(LSTM(128))
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
 
-optimizer = RMSprop(lr=0.01)
+optimizer = RMSprop(lr=0.001)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-
 
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
@@ -82,47 +113,36 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 # train the model, output generated text after each iteration
-for iteration in range(1, 50):
+for iteration in range(iterations):
     print()
     print('-' * 50)
     print('Iteration', iteration)
+
+    out.write('\n')
+    out.write('-' * 50)
+    out.write("\nIteration " + str(iteration))
+
     model.fit(x, y,
               batch_size=128,
-              epochs=5, validation_data=(x, y), callbacks=[history])
+              epochs=epochs, validation_data=(x, y), callbacks=[history])
 
-
-    plt.ion()
-    fig = plt.figure()
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper right')
-    plt.show()
+    All_history['acc'] += history.history['acc']
+    All_history['loss'] += history.history['loss']
 
     start_index = random.randint(0, len(text) - maxlen - 1)
     for diversity in [0.2, 0.5, 1.0, 1.2]:
         print()
         print('----- diversity:', diversity)
-        #out.write(str('----- diversity: ' + diversity))
+        out.write("----- diversity: " + str(diversity))
 
         generated = ''
         sentence = text[start_index: start_index + maxlen]
         generated += sentence
         print('----- Generating with seed: "' + sentence.replace('\n', '\\n') + '"')
         out.write(str('----- Generating with seed: "' + sentence.replace('\n', '\\n') + '"'))
-        #sys.stdout.write(generated)
+        # sys.stdout.write(generated)
 
-        for i in range(400):
+        for i in range(count_of_chars):
             x_pred = np.zeros((1, maxlen, len(chars)))
             for t, char in enumerate(sentence):
                 x_pred[0, t, char_indices[char]] = 1.
@@ -139,3 +159,18 @@ for iteration in range(1, 50):
             sys.stdout.flush()
             out.flush()
         print()
+
+plt.ion()
+fig = plt.figure()
+
+plt.plot(All_history['acc'])
+plt.title('model acc')
+plt.ylabel('acc')
+plt.xlabel('epoch')
+plt.show()
+
+plt.plot(All_history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.show()
